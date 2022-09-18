2d94cd13a318c0bc5e16e2f0d3bb9403bc00ac11
Noriega402 <d.noriegachajon@gmail.com>
2022-09-17 20:50:26 -0600

    Create insert folder

---
+++ inserts/insertsGames.sql
@@ -0,0 +1,17 @@
+SELECT insert_enterprise('NINTENDO');
+SELECT insert_enterprise('CAPCOM');
+SELECT insert_enterprise('SEGA');
+SELECT insert_enterprise('KONAMI');
+SELECT insert_enterprise('SQUARE');
+SELECT insert_enterprise('PLAY STATION');
+SELECT insert_enterprise('TECMO');
+SELECT insert_enterprise('TECHNOS JAPAN');
+SELECT insert_enterprise('SANTA MONICA');
+SELECT insert_enterprise('UBISOFT');
+SELECT insert_enterprise('BANDAL NAMCO');
+SELECT insert_enterprise('ACTIVISION BLIZZARD');
+SELECT insert_enterprise('ELECTRONICS ART');
+SELECT insert_enterprise('SQUARE ENIX');
+SELECT insert_enterprise('SONY');
+SELECT insert_enterprise('MICROSOFT');
+SELECT insert_enterprise('RAW FURY');
