ea7c3bb597aa95eb5ba439fa11355260ce5d78e1
Noriega402 <d.noriegachajon@gmail.com>
2022-09-18 18:29:56 -0600

    Add inserts with tables enterprise and games

--- inserts/insertsGames.sql
+++ inserts/insertsGames.sql
@@ -1,18 +1,10 @@
--- Data to insert in the game table
-SELECT insert_enterprise('NINTENDO');
-SELECT insert_enterprise('CAPCOM');
-SELECT insert_enterprise('SEGA');
-SELECT insert_enterprise('KONAMI');
-SELECT insert_enterprise('SQUARE');
-SELECT insert_enterprise('PLAY STATION');
-SELECT insert_enterprise('TECMO');
-SELECT insert_enterprise('TECHNOS JAPAN');
-SELECT insert_enterprise('SANTA MONICA');
-SELECT insert_enterprise('UBISOFT');
-SELECT insert_enterprise('BANDAL NAMCO');
-SELECT insert_enterprise('ACTIVISION BLIZZARD');
-SELECT insert_enterprise('ELECTRONICS ART');
-SELECT insert_enterprise('SQUARE ENIX');
-SELECT insert_enterprise('SONY');
-SELECT insert_enterprise('MICROSOFT');
-SELECT insert_enterprise('RAW FURY');
+SELECT *FROM games;
+SELECT insert_game('New Super Mario Bros','2012/08/23',1); --nintendo
+SELECT insert_game('Sonic The Hedgehog', '1998-05-20',3); --sega
+SELECT insert_game('FIFA 2004', '2004-08-29',6); --ps
+SELECT insert_game('Super Bomberman', '2017-12-25',4); -- konami
+SELECT insert_game('Resident Evil 4', '2005-10-05',2); --capcom
+SELECT insert_game('Super Mario Galaxi','2007-11-01',1); --nintendo
+SELECT insert_game('Super Mario Galaxi 2','2010-05-23',1); --nintendo
+SELECT insert_game('Resident Evil 2 Remake','2019-03-15',2);--capcom
+SELECT insert_game('','',);
