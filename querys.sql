c8446bfac1b502a171830d81003a433cd2799f21
Noriega402 <d.noriegachajon@gmail.com>
2022-09-20 18:03:45 -0600

    Modify select to enterprises

--- querys.sql
+++ querys.sql
@@ -2,15 +2,15 @@ SELECT *FROM enterprises;
 SELECT *FROM games;
 
 -- select company and game
-SELECT e.name_enterprises as enterprises, g.name_game as name
+SELECT e.name_enterprise as enterprise, g.name_game as name
 FROM games as g
-INNER JOIN enterprises as e ON e.id = g.id_enterprises;
+INNER JOIN enterprises as e ON e.id = g.id_enterprise;
 
 -- select the total number of sets belonging to X company
-SELECT COUNT(*) FROM games WHERE games.id_enterprises = 1;
+SELECT COUNT(*) FROM games WHERE games.id_enterprise = 1;
 
 -- concatenate company name and email
-SELECT CONCAT('Empresa: ',enterprises.name_enterprises,' Correo: ',enterprises.mail ) as contacto
+SELECT CONCAT('Empresa: ',enterprises.name_enterprise,' Correo: ',enterprises.mail ) as contacto
 FROM enterprises;
 
 -- select the games from 2000 onwards
@@ -22,6 +22,50 @@ SELECT COUNT(*) FROM games;
 -- Count the total number of registered companies
 SELECT COUNT(*) FROM enterprises;
 
+-- Sintax of Procedures Languages
+DO $$ --bloque de codigo ($$ abriendo simbolo del bloque de codigo)
+BEGIN -- comienzo del codigo
+	RAISE NOTICE 'Mensaje desde la consola de postgreSQL';
+END $$ -- fin del codigo ($$ cerrando simbolo del bloque de cdigo)
 
--- call function
+-- Procedure Languages
+DO $$
+DECLARE
+	obtener record; -- record es para almacenar los datos de una fila
+BEGIN
+	FOR obtener IN SELECT *FROM games LOOP -- for para recorrer toos los datos de la tabla games
+		RAISE NOTICE 'El nombre del juego es: %', obtener.name_game; --(% reeemplaza a obtener.name_game)
+	END LOOP; -- teminar el bucle
+END $$
+
+-- Procedure Languages 2
+DO $$
+DECLARE
+	des record;
+	counter integer := 0; --(:= es para asignar un valor propio de Postgres)
+BEGIN
+	FOR des IN SELECT *FROM games LOOP
+		RAISE NOTICE 'Nombre del juego: % el año lanzado es: %', des.name_game, des.fecha;
+		counter := counter + 1;
+	END LOOP;
+	RAISE NOTICE 'El total de juegos en la base de datos es de: %', counter;
+END $$
+
+-- Asignar a una funcion
+
+CREATE OR REPLACE FUNCTION listar_games()
+	RETURNS VOID
+AS $$
+DECLARE
+	des record;
+	counter integer := 0; --(:= es para asignar un valor propio de Postgres)
+BEGIN
+	FOR des IN SELECT *FROM games LOOP
+		RAISE NOTICE 'Nombre del juego: % el año lanzado es: %', des.name_game, des.fecha;
+		counter := counter + 1;
+	END LOOP;
+	RAISE NOTICE 'El total de juegos en la base de datos es de: %', counter;
+END
+$$ LANGUAGE PLPGSQL;
+
 SELECT listar_games(); -- ver en Messages
