8776ebac6d417b704665611b2c3fb533292ff933
Noriega402 <d.noriegachajon@gmail.com>
2022-09-23 12:44:06 -0600

    Add function listar_games

--- functions.sql
+++ functions.sql
@@ -15,3 +15,22 @@ BEGIN
 	RETURN QUERY SELECT *FROM enterprises WHERE name_enterprise = name;
 END
 $$ LANGUAGE PLPGSQL;
+
+
+-- create function to list games
+CREATE OR REPLACE FUNCTION listar_games()
+	RETURNS VOID
+AS $$
+DECLARE
+	des record;
+	counter integer := 0; --(:= es para asignar un valor propio de Postgres)
+BEGIN
+	FOR des IN SELECT *FROM games LOOP
+		RAISE NOTICE 'Nombre del juego: % el año lanzado es: %', des.name_game, des.fecha;
+		counter := counter + 1;
+	END LOOP;
+	RAISE NOTICE 'El total de juegos en la base de datos es de: %', counter;
+END
+$$ LANGUAGE PLPGSQL;
+
+SELECT listar_games(); -- ver en Messages
